11effa7252679743c9eef0878f48783a2bb623d8
amoore <amoore@franklinamerican.com>
2017-05-19 17:45:11 -0500

    nav works now and dupe sorted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    // templateUrl: './app.component.html',
+    template: `<app-nav></app-nav>`,
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import { JobsComponent } from './jobs/jobs.component';
 import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
 
 const appRoutes: Routes = [
     {path: 'jobs', component: JobsComponent},
     {path: 'about', component: AboutComponent},
-    {path: '', component: AppComponent}
+    {path: '', component: HomeComponent}
 ];
 
 @NgModule({
     declarations: [
         AppComponent,
         JobsComponent,
-        AboutComponent
+        AboutComponent,
+        NavComponent,
+        HomeComponent
     ],
     imports: [
         RouterModule.forRoot(appRoutes),
